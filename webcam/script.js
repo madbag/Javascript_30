17a3c7e1e3c38b6cebd662189f436a5eacdec57b
Madhushree B <boylemadhushree@gmail.com>
2025-01-23 20:31:16 +0100

    cannot get the height and width of the video and the canvas

---
+++ webcam/script.js
@@ -0,0 +1,37 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(localMediaStream => {
+        // console.log(localMediaStream);
+        video.srcObject = localMediaStream;
+        // video.src = window.URL.createObjectURL(localMediaStream);
+        video.play()
+    })
+    .catch(err => console.error(`Ohhhhhnooo!`, err))
+}
+
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    // canvas.width = width;
+    // canvas.height = height;
+    console.log(width, height)
+
+//    return setInterval(() => {
+//         ctx.drawImage(video, 0, 0, width, height);
+//         // let pixels = ctx.getImageData(0, 0, width, height);
+//         // console.log(pixels);
+//     }, 16)
+}
+
+getVideo();
+// console.log(paintToCanvas());
+
+video.addEventListener('camplay', paintToCanvas);
